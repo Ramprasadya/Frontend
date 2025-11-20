029639f1660b6e330eb5517479d14e8b95c3684b
Ramprasad yadav <yadavramprasad563@gmail.com>
2025-11-20 09:07:40 +0530

    frontend

---
+++ config.js
@@ -0,0 +1,16 @@
+// for Production purpose
+// export const serverUrl = "https://tapop.onrender.com"; // Render
+// export const serverUrl = "https://server.qviq.io"; // AWS
+// export const clientUrl = "https://qviq.io";
+// export const hostname = "qviq.io";
+
+// for Development purpose
+//  export const serverUrl = "http://localhost:2610";
+//  export const clientUrl = "http://localhost:3000";
+//  export const hostname = "localhost:3000";
+
+
+// for test Deployment on Vercel purpose
+export const serverUrl = "https://qviqbackend.onrender.com";
+export const clientUrl = "https://ramprasad.site";
+export const hostname = "ramprasad.site";
